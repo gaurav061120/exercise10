b1017d62a72b465e7752a20e16176a4b13447c14
gaurav061120 <gaurav.mishra0611@gmail.com>
2023-07-21 11:34:28 +0530

    initial commit

---
+++ ConsoleApp1/LargeDataCollection.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class LargeDataCollection : IDisposable
+    {
+
+
+        int[] data;
+        public LargeDataCollection(int[] initialData)
+        {
+            data = new int[initialData.Length];
+            initialData.CopyTo(data, 0);
+
+        }
+        public void AddElement(int element)
+        {
+            Array.Resize(ref data, data.Length + 1);
+            data[data.Length - 1] = element;
+        }
+
+        public void Dispose()
+        {
+            
+            data = null;
+            GC.SuppressFinalize(this);
+        }
+
+        public void RemoveElement(int element)
+        {
+            int index = Array.IndexOf(data, element);
+            if (index >= 0)
+            {
+                for (int i = index; i < data.Length - 1; i++)
+                {
+                    data[i] = data[i + 1];
+                }
+                Array.Resize(ref data, data.Length - 1);
+            }
+        }
+        public void PrintElements()
+        {
+            Console.WriteLine("element in the collection");
+            foreach (var element in data)
+            {
+                Console.WriteLine(element + " ");
+
+            }
+            Console.WriteLine();
+
+        }
+    
+           
+        }
+    }
+
+
+    
+
+
+
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+class program
+{
+    static void Main(string[]args)
+    {
+        int[] initialData = { 10, 20, 30, 40, 50 };
+        using (var largeDataCollection = new LargeDataCollection(initialData))
+        {
+            largeDataCollection.PrintElements();
+            largeDataCollection.AddElement(60);
+            largeDataCollection.AddElement(70);
+            largeDataCollection.PrintElements();
+           
+            largeDataCollection.RemoveElement(30);
+            largeDataCollection.PrintElements();
+           
+            Console.WriteLine("program complete");
+        }
+    }
+}
+
+  
+    
+    
+        
